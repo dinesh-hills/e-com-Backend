70b9d276d20bb8359cfeae2e1f23643692974d70
Dinesh Ramasamy <dinesh+work@collis.in>
2022-11-21 16:19:41 +0530

    Postgres and Models
    - Setup with postgresdb
    - install psy2copy dependency
    - Add slug field to product
    - change price to unit_price
    - makemigrations files included

--- Ecom_store/settings.py
+++ Ecom_store/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import pg_pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'debug_toolbar',
     'trail',
+    'store',
+    'tag',
 ]
 
 MIDDLEWARE = [
@@ -84,8 +87,12 @@ WSGI_APPLICATION = 'Ecom_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'storefront3',
+        'USER': pg_pass.user,
+        'PASSWORD': pg_pass.password,
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- store/models.py
+++ store/models.py
@@ -7,8 +7,9 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField(default='-')
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     
@@ -60,4 +61,4 @@ class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=255)
-    pincode = models.PositiveSmallIntegerField(max_length=6)
+    pincode = models.PositiveSmallIntegerField()

cce486de8f58e1ee38404a4d8d2fc591177ef78b
Dinesh Ramasamy <dinesh+work@collis.in>
2022-12-05 12:38:50 +0530

    Minor fixes and new endpoint
    - collections endpoint added.
    - minor fixes on models.

--- store/views.py
+++ store/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
 # Create your views here.
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True, context={'request':request})
-    return Response(serializer.data)
-
+    if request.method == 'GET':
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True, context={'request':request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
 @api_view()
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
@@ -18,5 +25,27 @@ def product_detail(request, pk):
     return Response(serializer.data)
 
 @api_view()
+def collection_list(request):
+    if request.method == 'GET':
+        queryset = Collection.objects.prefetch_related('product_set').all()
+        serializer = CollectionSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = CollectionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def collection_detail(request, pk):
-    return Response("ok")
+    if request.method == 'GET':
+        collection = get_object_or_404(Collection, pk=pk)
+        serializer = CollectionSerializer(collection)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = CollectionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    elif request.method == 'DELETE':
+        return Response("ok")

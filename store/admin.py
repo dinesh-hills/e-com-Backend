022c3f911f38733e5b5d2f0a96e7075fd9d8e285
Dinesh Ramasamy <dinesh+work@collis.in>
2022-12-13 09:49:27 +0530

    Config Admin site

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product, Customer, Order
 
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price', 'inventory', 'collection']
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_select_related = ['user']
+    
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['placed_at']
--- store/models.py
+++ store/models.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.contrib import admin
 from django.db import models
 from django.core.validators import MinValueValidator
 from uuid import uuid4
@@ -24,6 +26,9 @@ class Product(models.Model):
     promotions = models.ManyToManyField(Promotion, blank=True)
     rating = None
     
+    def __str__(self) -> str:
+        return self.title
+    
     
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -36,13 +41,21 @@ class Customer(models.Model):
         (MEMBERSHIP_GOLD, 'Gold')
     ]
     
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default='B')
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'{self.user.first_name} {self.user.last_name}'
     
+    @admin.display(ordering='user__first_name')
+    def first_name(self):
+        return self.user.first_name
+    
+    @admin.display(ordering='user__last_name')
+    def last_name(self):
+        return self.user.last_name
     
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
@@ -58,6 +71,7 @@ class Order(models.Model):
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)

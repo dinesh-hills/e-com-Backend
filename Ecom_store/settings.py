67a3c4083081896e1159e7c6e0a7dc8a76753ede
Dinesh Ramasamy <dinesh+work@collis.in>
2022-12-13 15:15:45 +0530

    Get current_user
    - Config current_user seriazlier.
    - Add current_user endpoint for store.

--- Ecom_store/settings.py
+++ Ecom_store/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 import configparser
 
@@ -151,6 +152,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'AUTH_HEADER_TYPES': ('JWT',),
 }
 
@@ -159,5 +161,6 @@ AUTH_USER_MODEL = 'core.User'
 DJOSER = {
     'SERIALIZERS': {
         'user_create': 'core.serializers.UserCreateSerializer',
+        'current_user': 'core.serializers.UserSerializer',
     }
 }
--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,10 @@
 from djoser.serializers import UserCreateSerializer as BaseUserCreateSeriazlier
+from djoser.serializers import UserSerializer as BaseUserSerializer
 
 class UserCreateSerializer(BaseUserCreateSeriazlier):
     class Meta(BaseUserCreateSeriazlier.Meta):
         fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name']
+        
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']

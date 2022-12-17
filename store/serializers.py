952dffa8b023eae10297c50401b1549d5cf1a902
Dinesh Ramasamy <dinesh+work@collis.in>
2022-12-17 11:37:09 +0530

    Add dependencies for running background tasks
    - Redis and Celery add to prod dependencies
    - Setup celery

--- store/serializers.py
+++ store/serializers.py
@@ -16,10 +16,22 @@ class CollectionSerializer(serializers.ModelSerializer):
         return collection.product_set.count()
     
 
+class ProductImageSerializer(serializers.ModelSerializer):
+    
+    def create(self, validated_data):
+        product_id = self.context['product_id']
+        return ProductImage.objects.create(product_id=product_id, **validated_data)
+    
+    class Meta:
+        model = ProductImage
+        fields = ['id', 'image']
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True, read_only=True)
     class Meta:
         model = Product
-        fields = ['id', 'title', 'slug', 'description', 'inventory', 'unit_price', 'price_with_tax', 'collection']
+        fields = ['id', 'title', 'slug', 'description', 'inventory', 'unit_price', 'price_with_tax', 'collection', 'images']
     
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     
@@ -166,13 +178,3 @@ class CreateOrderSerializer(serializers.Serializer):
             
             return order
         
-        
-class ProductImageSerializer(serializers.ModelSerializer):
-    
-    def create(self, validated_data):
-        product_id = self.context['product_id']
-        return ProductImage.objects.create(product_id=product_id, **validated_data)
-    
-    class Meta:
-        model = ProductImage
-        fields = ['id', 'image']
